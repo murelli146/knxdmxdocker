0a5ed69d5235e2965f268c4912a6a704f0e0b54f
Gernot Klobucaric <gernot-tv@gmx.at>
2021-03-25 21:46:13 +0100

    knxd integriert

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,44 @@
+FROM debian:latest as builder
+
+ARG BUILD_BRANCH
+RUN echo BUILD_BRANCH: ${BUILD_BRANCH}
+
+ENV BUILD_PACKAGES \
+  autoconf \
+  automake \
+  autotools-dev \
+  build-essential \
+  cmake \
+  debhelper \
+  dh-systemd \
+  git-core \
+  libev-dev \
+  libsystemd-dev \
+  libtool \
+  libusb-1.0-0-dev \
+  pkg-config
+
+RUN apt-get update && apt-get -y install $BUILD_PACKAGES
+
+# build knxd
+
+RUN git clone https://github.com/knxd/knxd.git && cd knxd && git checkout ${BUILD_BRANCH} \
+  && dpkg-buildpackage -b -uc
+
+# install in a fresh and thus clean environment
+
 FROM debian:latest
-MAINTAINER Gernot Klobucaric <murelli146>
+
+COPY --from=builder /knxd_*.deb /knxd-tools_*.deb ./
+RUN apt-get update && apt-get -y install libev4 libusb-1.0-0 init-system-helpers \
+  && dpkg -i knxd_*.deb knxd-tools_*.deb
+
 COPY root / 
-ENV IPEIBD 192.168.179.62
-ENV IPE131 192.168.179.214
+
+EXPOSE 3672 6720
+
+ENV IPKNXGW 192.168.179.62
+ENV IPDMXE131 192.168.179.214
+ENV EIBADDR 1.1.200
+ENV CLIENTADDRS 1.1.201:5
 ENTRYPOINT ["sh","/etc/knxdmxd/run.sh"]
--- root/etc/knxdmxd/run.sh
+++ root/etc/knxdmxd/run.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+# knxd starten
+#/usr/bin/knxd /etc/knxd/knxd.ini
+/usr/bin/knxd --daemon --pid-file=/var/run/knxd.pid --trace=0x0 --error=0x9 --eibaddr=$EIBADDR --client-addrs=$CLIENTADDRS --GroupCache=2000 --listen-tcp=6720 --layer2=ipt:$IPKNXGW
+status_knx=$?
 # knxdmxd starten ohne Debug als Deamon
-/usr/bin/knxdmxd -d -c /etc/knxdmxd/knxdmxd.conf -u ip:$IPEIBD -p /var/run/knxdmxd.pid -r $IPE131
+/usr/bin/knxdmxd -d -c /etc/knxdmxd/knxdmxd.conf -u ip:127.0.0.1 -p /var/run/knxdmxd.pid -r $IPDMXE131
 # knxdmxd starten mit Debug
 #/usr/bin/knxdmxd -c /etc/knxdmxd/knxdmxd.conf -u ip:$IPEIBD -p /var/run/knxdmxd.pid -r $IPE131
-status=$?
-# Prüfen ob gestartet, sonst den Container beenden.
-if [ $status -ne 0 ];then
+status_dmx=$?
+# Prüfen ob knxd gestartet, sonst den Container beenden.
+if [ $status_knx -ne 0 ];then
+	echo "Failed to start knxd"
+	exit 0
+else
+ echo "knxd gestartet"
+fi
+# Prüfen ob knxdmxd gestartet, sonst den Container beenden.
+if [ $status_dmx -ne 0 ];then
 	echo "Failed to start knxdmxd"
 	exit 0
 else
@@ -13,10 +24,16 @@ else
 fi
 # Prüfintervall ob PIDfile vorhanden sonst Container beenden
 while sleep 600; do
-	if [ -e /var/run/knxdmxd.pid ]; then
-		echo "PID OK"
+	if [ -e /var/run/knxd.pid ]; then
+		:
 	else
-		echo "exit 0"
+		echo "exit 0 : knxd.pid nicht vorhanden"
+		exit 0
+	fi
+	if [ -e /var/run/knxdmxd.pid ]; then
+		:
+	else
+		echo "exit 0 : knxdmxd.pid nicht vorhanden"
 		exit 0
 	fi
 done
